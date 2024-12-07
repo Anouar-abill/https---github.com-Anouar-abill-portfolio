119637a6f0c374c666e1bfa87dd23ae1cb73a4cd
Anouar Abillouch <anouar.abillouch@hotmail.com>
2024-12-07 17:01:08 +0100

    map

--- js/map.js
+++ js/map.js
@@ -1,21 +1,15 @@
-
-
-
-
-let map = L.map("map").setView([51.2399, 4.4212], 15);
+let map = L.map("map").setView([51.2554, 4.4339], 15);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
+  maxZoom: 10,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-let marker = L.marker([51.2399, 4.4212]).addTo(map);
-marker
-  .bindPopup("<b>AP Hogeschool</b><br>Noorderlaan, Antwerpen.")
-  .openPopup();
+let marker = L.marker([51.2554, 4.4339]).addTo(map);
+marker.bindPopup("<b>Merksem</b><br>Antwerpen.").openPopup();
 
-let circle = L.circle([51.2399, 4.4212], {
-  className: "circle-style", // Voeg hier de CSS-klasse toe
+let circle = L.circle([51.2554, 4.4339], {
+  className: "circle-style",
   radius: 300,
 }).addTo(map);
